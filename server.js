f64a3e0abe0cf7efa7980b590cfef29aa3945871
Justin Beckwith <justin.beckwith@gmail.com>
2018-04-25 09:55:56 -0700

    read creds from env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const {google} = require('googleapis');
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
 
 const app = express()
 app.get('/', async (req, res) => {
@@ -13,9 +14,21 @@ app.listen(3000, () => {
   console.log('Listening on port 3000.');
 });
 
+function getCredentials() {
+  const filePath = path.join(__dirname, 'keys.json');
+  if (fs.existsSync(filePath)) {
+    return require(filePath);
+  }
+  if (process.env.CREDENTIALS) {
+    return JSON.parse(process.env.CREDENTIALS);
+  }
+  throw new Error('Unable to load credentials');
+}
+
 async function getFiles () {
+  const credentials = getCredentials();
   const client = await google.auth.getClient({
-    keyFile: path.join(__dirname, 'keys.json'),
+    credentials,
     scopes: 'https://www.googleapis.com/auth/drive.readonly'
   });
   const drive = google.drive({
@@ -24,4 +37,3 @@ async function getFiles () {
   });
   return drive.files.list();
 }
-
